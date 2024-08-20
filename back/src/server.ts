6d5e2404d0f621e1a18fc6d12e439f0a00ddc85c
AleCejas <alejandro.06555@gmail.com>
2024-08-20 19:51:30 -0300

    Ultimos cambios en el back

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,9 +1,16 @@
 import express, { Express } from "express";
 import router from "./routes/indexRouter";
+import morgan from "morgan" 
+import cors from "cors"
 
 
 const app: Express = express();
+
+
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(cors())
+
 
 app.use(router)
 
