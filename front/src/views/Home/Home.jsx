140a1b2ef49e596499b7e75f64c94f174939ac58
AleCejas <alejandro.06555@gmail.com>
2024-08-21 21:43:34 -0300

    Detalles agregados al front, estilos e imagen al home

--- front/src/views/Home/Home.jsx
+++ front/src/views/Home/Home.jsx
@@ -1,11 +1,17 @@
-
+import image from "../../assets/turnos.jpg"
+import styles from "./Home.module.css"
 
 
 const Home = () => {
     return (
-        <>
+        <div className={styles.containerHome}>
             <h1 style={{ textAlign: "center" }}>HOME</h1>
-        </>
+            <img src={image} alt="imagen de turnos" />
+            <p>Agendá tu turno con la mejor atención</p>
+            <p>Para agendar y ver tus turnos inicia sesión</p>
+            <p>Si todavía no eres usuario, puedes registrarte</p>
+
+        </div>
 
     )
 }
