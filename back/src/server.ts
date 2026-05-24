18e74c30d4cd9bb1615ffcdab46fc8de3fa637d3
AleCejas <alejandro.06555@gmail.com>
2026-05-24 00:02:13 -0300

    Cambios en configuracion

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
 import router from "./routes/indexRouter";
-import morgan from "morgan" 
+import morgan from "morgan"
 import cors from "cors"
 
 
@@ -9,7 +9,22 @@ const app: Express = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors())
+
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://pm-3-alejandro-m-cejas-k9tv12rg8.vercel.app/"
+]
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("No permitido por CORS"))
+        }
+    },
+    credentials: true
+}))
 
 
 app.use(router)
